6642796250e53abfbe47b31311b95f9393385a5e
ivanbrtka <60200942+ivanbrtka@users.noreply.github.com>
2024-10-22 19:16:57 +0200

    Update __init__.py

--- custom_components/hello-world/__init__.py
+++ custom_components/hello-world/__init__.py
@@ -1,28 +1,7 @@
-"""
-The "hello world" custom component.
+import logging
 
-This component implements the bare minimum that a component should implement.
+_LOGGER = logging.getLogger(__name__)
 
-Configuration:
-
-To use the hello_world component you will need to add the following to your
-configuration.yaml file.
-
-hello_world:
-"""
-from __future__ import annotations
-
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-
-# The domain of your component. Should be equal to the name of your component.
-DOMAIN = "hello_world"
-
-
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up a skeleton component."""
-    # States are in the format DOMAIN.OBJECT_ID.
-    hass.states.set('hello_world.Hello_World', 'Works!')
-
-    # Return boolean to indicate that initialization was successfully.
+def setup(hass, config):
+    _LOGGER.info("Hello World from the custom component!")
     return True
