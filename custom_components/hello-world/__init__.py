5ed90e79a922810e5388118bf35b4e3f3db83dc3
ivan.brtka <ivan.brtka@student.tuke.sk>
2024-11-27 20:59:58 +0100

    Changed init.py

--- custom_components/hello-world/__init__.py
+++ custom_components/hello-world/__init__.py
@@ -1,11 +1,10 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
-from homeassistant.helpers.discovery import load_platform
 
 DOMAIN = "power_outages"
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up the integration."""
     # Register a service for updating location
-    load_platform("sensor", DOMAIN, {}, config)
+    hass.data[DOMAIN] = {}
     return True
