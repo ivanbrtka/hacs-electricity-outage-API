d6c7329cb5ccd0bd0bf3edb334b2063a3c2be89c
ivan.brtka <ivan.brtka@student.tuke.sk>
2024-11-27 21:57:29 +0100

    added date functionality to sensor

--- custom_components/hello-world/sensor.py
+++ custom_components/hello-world/sensor.py
@@ -31,11 +31,54 @@ def setup_platform(
 # Sensor used for representing next electricity outage
 class PowerOutageSensor(SensorEntity):
 
+    # Use GPS data from Home Assistant config data
+    latitude = 17.41
+    longitude = 15.23
+
     # Define basic attributes
-    _attr_name = "Example Temperature"
-    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_name = "Next Power Outage Start Date"
+    _attr_device_class = SensorDeviceClass.DATE
     _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self):
+        
+        # URL of power outage data API and reverse GPS Lookup API
+        REVERSE_GPS_URL=f"https://nominatim.openstreetmap.org/reverse.php?lat={self._latitude}&lon={self._longtitude}&zoom=18&format=jsonv2"
+        OUTAGE_API_URL="https://www.vypadokelektriny.sk/api/data/outages30days/address"
+
+        try:
+            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+            gps_data = requests.get(REVERSE_GPS_URL, headers=headers, timeout=10)
+            gps_data_json = json.loads(gps_data.text)
+
+            postcode = gps_data_json['address']['postcode']
+            city = gps_data_json['address']['village']
+            postcode = postcode.replace(" ", "")
+
+            home_location_params = {
+                "data": {
+                    "OBEC":f"{city}",
+                    "PSC":f"{postcode}",
+                    "ULICA":"",
+                    "CISLO_DOMU":"",
+                    "EIC":"",
+                    "CISLO_ELEKTROMERA":""  
+                    }
+                }
+            response = requests.post(OUTAGE_API_URL, json=home_location_params, headers=headers, timeout=10)
+            response.raise_for_status()
+
+            outage_data_json = json.loads(response.text)
+
+            if outage_data_json:
+                next_electricity_outage_start = outage_data_json[0]['realStart']
+                self._attr_native_value = dt = datetime.strptime(next_electricity_outage_start, '%Y-%m-%dT%H:%M:%S%z')
+            else:
+                self._attr_native_value = None
+        
+        except Exception as e:
+            _LOGGER.error("Error fetching power outage data: %s", e)
+            self._state = "Error"
 '''
     # Fetch new data from API 
     def update(self):
