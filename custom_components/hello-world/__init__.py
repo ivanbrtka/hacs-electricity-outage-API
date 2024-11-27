c832f6bf0b63f4c18a8e599b915d6b014ab0833d
ivan.brtka <ivan.brtka@student.tuke.sk>
2024-11-27 19:30:54 +0100

    HA not showing sensor fix

--- custom_components/hello-world/__init__.py
+++ custom_components/hello-world/__init__.py
@@ -4,7 +4,6 @@ from homeassistant.helpers.typing import ConfigType
 DOMAIN = "power_outages"
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
-    """Set up the integration."""
-    # Register a service for updating location (voliteľné)
-    hass.states.async_set(f"{DOMAIN}.status", "No data")
+    """Set up the power outages integration."""
+    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
     return True
