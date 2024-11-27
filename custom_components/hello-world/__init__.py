e9d78b8d85a82079d91370e6a867c1a023646046
ivan.brtka <ivan.brtka@student.tuke.sk>
2024-11-27 20:55:10 +0100

    trying load_platform

--- custom_components/hello-world/__init__.py
+++ custom_components/hello-world/__init__.py
@@ -1,10 +1,11 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.discovery import load_platform
 
 DOMAIN = "power_outages"
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up the integration."""
-    # Register a service for updating location (voliteľné)
-    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
+    # Register a service for updating location
+    load_platform("sensor", DOMAIN, {}, config)
     return True
